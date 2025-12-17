9db9db0f9ea5816d52446ce60428ce360b843115
dus_fetch_data <68933518+dmitryusaty7@users.noreply.github.com>
2025-12-18 00:00:13 +1000

    Добавлена загрузка фото в Directus

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,13 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api')
 const { registerStartHandler } = require('./handlers/start.handler')
+const { registerPhotoHandler } = require('./handlers/photo.handler')
 
 // Создаём экземпляр бота и регистрируем обработчики
-function createBot({ token, logger, repositories, messages }) {
+function createBot({ token, logger, repositories, messages, directusClient }) {
   const bot = new TelegramBot(token, { polling: true })
 
   registerStartHandler({
     bot,
     brigadiersRepo: repositories.brigadiers,
+    logger,
+    messages,
+  })
+
+  registerPhotoHandler({
+    bot,
+    directusClient,
     logger,
     messages,
   })
