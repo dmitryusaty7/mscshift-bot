3b39ca9dc8e82318ba0eaebbc9997536b486c1c4
dus_fetch_data <68933518+dmitryusaty7@users.noreply.github.com>
2025-12-18 03:21:14 +1000

    Реализация блока регистрации

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const { registerStartHandler } = require('./handlers/start.handler')
 const { registerPhotoHandler } = require('./handlers/photo.handler')
+const { registerRegistrationModule } = require('../modules/register')
 
 // Создаём экземпляр бота и регистрируем обработчики
 function createBot({ token, logger, repositories, messages, directusClient }) {
@@ -13,13 +14,20 @@ function createBot({ token, logger, repositories, messages, directusClient }) {
     messages,
   })
 
+  registerRegistrationModule({
+    bot,
+    brigadiersRepo: repositories.brigadiers,
+    logger,
+    messages,
+  })
+
   registerPhotoHandler({
     bot,
     directusClient,
     logger,
     messages,
   })
-  
+
   bot.on('polling_error', (error) => {
     logger.error('Ошибка long polling', { error: error.message })
   })
